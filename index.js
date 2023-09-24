de09749f3866bbced70a8b7262d1c007109740a3
hrfahimm <fahimtaj10@gmail.com>
2023-09-24 20:16:32 +0600

    pay

--- index.js
+++ index.js
@@ -225,17 +225,46 @@ async function run() {
             const users = await usersCollection.estimatedDocumentCount();
             const products = await menuCollection.estimatedDocumentCount();
             const orders = await paymentCollection.estimatedDocumentCount();
+
+            // best way to get sum of the price field is to use group and sum operator
+            /*
+              await paymentCollection.aggregate([
+                {
+                  $group: {
+                    _id: null,
+                    total: { $sum: '$price' }
+                  }
+                }
+              ]).toArray()
+            */
+
             const payments = await paymentCollection.find().toArray();
             const revenue = payments.reduce(
                 (sum, payment) => sum + payment.price,
                 0
             );
-            res.send({ users, products, orders, revenue });
-        });
 
-        //
-        //
-        //
+            res.send({
+                revenue,
+                users,
+                products,
+                orders,
+            });
+        });
+        /**
+         * ---------------
+         * BANGLA SYSTEM(second best solution)
+         * ---------------
+         * 1. load all payments
+         * 2. for each payment, get the menuItems array
+         * 3. for each item in the menuItems array get the menuItem from the menu collection
+         * 4. put them in an array: allOrderedItems
+         * 5. separate allOrderedItems by category using filter
+         * 6. now get the quantity by using length: pizzas.length
+         * 7. for each category use reduce to get the total amount spent on this category
+         *
+         */
+
         app.get("/order-stats", async (req, res) => {
             const pipeline = [
                 {
@@ -265,6 +294,10 @@ async function run() {
                     },
                 },
             ];
+            const result = await paymentCollection
+                .aggregate(pipeline)
+                .toArray();
+            res.send(result);
         });
 
         //
